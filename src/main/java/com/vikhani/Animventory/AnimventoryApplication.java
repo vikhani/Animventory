aebdea83d10914ea1b074aeda470b986b5f10837
vikhani <vikhanieva@gmail.com>
2022-07-01 13:06:13 +0300

    Initial commit

---
+++ src/main/java/com/vikhani/Animventory/AnimventoryApplication.java
@@ -0,0 +1,13 @@
+package com.vikhani.Animventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikhani/Animventory/AnimventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikhani.Animventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
